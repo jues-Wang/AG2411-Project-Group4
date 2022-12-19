58c32fec2dadcaaa7ea6d291bae9312e25559294
cvmannen <cv.bostrom_@live.se>
2022-12-19 18:47:23 +0100

    zoom, full extent, panning around

--- AG2411_raster/src/se/kth/ag2411/mapalgebra/ZonalWindow.java
+++ AG2411_raster/src/se/kth/ag2411/mapalgebra/ZonalWindow.java
@@ -269,14 +269,24 @@ public class ZonalWindow extends JFrame {
 				TestGUI.imageList.add(outputLayer.toImage());
 				TestGUI.layerNameList.addElement(outLayerName);
 				
-				TestGUI.mPanel = new MapPanel(outputLayer.toImage(), scale);
 				TestGUI.aboveLayer = outputLayer;
-				TestGUI.mPanel.repaint();
+				
+				TestGUI.layeredPane.remove(TestGUI.mPanel);
+				
+				TestGUI.mPanel = new MapPanel(outputLayer.toImage(), scale);
+				
+				TestGUI.getMapStartX();
+				TestGUI.getMapStartY();
+				TestGUI.mPanel.setBounds(TestGUI.mapStartX, TestGUI.mapStartY, 2000, 2000);	
+				TestGUI.mPanel.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				
+				TestGUI.layeredPane.add(TestGUI.mPanel);
+				TestGUI.layeredPane.revalidate();
+				TestGUI.layeredPane.repaint();
 				
 				outputLayer.save(outputFileName);
 				
 				newWindow.dispose();
-				// HERE IS A LOT STILL MISSING
 			}
 		});
 
